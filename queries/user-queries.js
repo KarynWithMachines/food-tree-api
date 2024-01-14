5bc281dffdd5cfe90fea71fffe060d79318daaa9
Karyn Huston <huston.karyn@gmail.com>
2024-01-13 23:24:07 -0500

    queries for general users

--- queries/user-queries.js
+++ queries/user-queries.js
@@ -1,3 +1,24 @@
 const db = require("../db/db-config.js");
 
-module.exports = {};
+const display_tree = async (id) => {
+  try {
+    const get_tree_details = await db.oneOrNone("SELECT * FROM account WHERE id=$1",id);
+
+    return get_tree_details;
+
+  } catch (error) {
+    return error;
+  }
+};
+
+const view_recipe = async (id) => {
+  try {
+    const get_recipe_details = await db.oneOrNone("SELECT * FROM recipe WHERE id=$1",id);
+    
+    return get_recipe_details;
+
+  } catch (error) {
+    return error;
+  }
+};
+module.exports = { view_tree, view_recipe };
